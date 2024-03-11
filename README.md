0eb675ed09951f507e68a0cef35e1dac408fce4d
baniramdlan <baniramdlan@gmail.com>
2024-03-11 16:56:01 +0700

    ## 0.0.1
    * DataAddPage

--- README.md
+++ README.md
@@ -1,39 +1,57 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
 ```dart
-const like = 'sample';
+import 'package:devaloop_data_add_page/data_add_page.dart';
+import 'package:devaloop_form_builder/form_builder.dart';
+import 'package:devaloop_form_builder/input_field_text.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DataAddPage(
+        title: 'Add Doctor',
+        subtitle: 'Add Doctor',
+        inputFields: const [
+          InputText(
+            name: 'name',
+            label: 'Name',
+          ),
+          InputText(
+            name: 'email',
+            label: 'Email (Google Account)',
+            inputTextMode: InputTextMode.email,
+          ),
+        ],
+        onAfterValidation: (context, inputValues, isValid, erorMessage) {
+          if (isValid) {
+            if (!inputValues['email']!.getString()!.contains('gmail.com')) {
+              erorMessage['email'] = 'Email must an google account (gmail.com)';
+            }
+          }
+        },
+        add: (inputValues) => Future(() async {
+          // ignore: unused_local_variable
+          var name = inputValues['name']!.getString()!;
+          // ignore: unused_local_variable
+          var email = inputValues['email']!.getString()!;
+
+          await Future.delayed(Durations.extralong4);
+        }),
+      ),
+    );
+  }
+}
 ```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
--- lib/data_add_page.dart
+++ lib/data_add_page.dart
@@ -1,7 +1,114 @@
-library data_add_page;
+library devaloop_data_add_page;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:devaloop_form_builder/form_builder.dart';
+import 'package:flutter/material.dart';
+
+class DataAddPage extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final List<Input> inputFields;
+  final dynamic Function(Map<String, InputValue> inputValues) add;
+  final Function(BuildContext context, Map<String, InputValue> inputValues)?
+      onInitial;
+  final dynamic Function(
+      BuildContext context,
+      Map<String, InputValue> inputValues,
+      bool isValid,
+      Map<String, String?> errorMessages)? onAfterValidation;
+  final dynamic Function(
+          BuildContext context, Map<String, InputValue> inputValues)?
+      onBeforeValidation;
+  final dynamic Function(
+      BuildContext context,
+      Input input,
+      dynamic previousValue,
+      dynamic currentValue,
+      Map<String, InputValue> inputValues)? onValueChanged;
+  final bool? isFormEditable;
+  final List<AdditionalButton>? additionalButtons;
+
+  const DataAddPage(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      required this.inputFields,
+      required this.add,
+      this.onInitial,
+      this.onAfterValidation,
+      this.onBeforeValidation,
+      this.onValueChanged,
+      this.isFormEditable,
+      this.additionalButtons});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          title: Text(
+            title,
+            overflow: TextOverflow.ellipsis,
+          ),
+          subtitle: Text(
+            subtitle,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: FormBuilder(
+            inputFields: inputFields,
+            onInitial: onInitial,
+            onAfterValidation: onAfterValidation,
+            onBeforeValidation: onBeforeValidation,
+            onValueChanged: onValueChanged,
+            isFormEditable: isFormEditable,
+            additionalButtons: additionalButtons,
+            resetToInitialAfterSubmit: true,
+            onSubmit: (context, inputValues) async {
+              var result = await add.call(inputValues);
+
+              if (!context.mounted) return;
+
+              var confirm = await showDialog<String>(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    icon: const Icon(Icons.info),
+                    title: const Text('Data successfully added.'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Add Another'),
+                        onPressed: () {
+                          Navigator.of(context).pop('Add Another');
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('Ok'),
+                        onPressed: () {
+                          Navigator.of(context).pop('Ok');
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+
+              if (confirm == 'Ok') {
+                if (!context.mounted) return;
+                Navigator.pop(context, result);
+              }
+            },
+            submitButtonSettings: const SubmitButtonSettings(
+              label: 'Add',
+              icon: Icon(Icons.add),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
